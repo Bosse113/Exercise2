d8876bea1fa802475a741e1bb5738f45a60c9eb6
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-05-21 15:05:23 +0200

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool meny = true;
+
+            while (meny == true)
+            {
+                // MENY
+                Console.WriteLine("--- Huvudmeny ---");
+                Console.WriteLine("1. Ålderskategori");
+                Console.WriteLine("0. Avsluta");
+                Console.Write("Välj: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        CheckAge();
+                        break;
+
+
+                    case "0":
+                        meny = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Felaktig inmatning");
+                        break;
+                }
+
+            }
+        }
+
+        private static void CheckAge()
+        {
+            //Kolla om ungdom <20 (80kr) eller pensionär >64 (90kr) annars 120 kr
+            Console.Write("Skriv in åldern på personen:");
+            int age = int.Parse(Console.ReadLine());
+            if (age < 20)
+            {
+                Console.WriteLine("Ungdomspris:80 kr.");
+            }
+            else if (age > 64)
+            {
+                Console.WriteLine("Pensionärspris:90 kr.");
+            }
+            else
+            {
+                Console.WriteLine("Standardpris:120 kr.");
+            }
+        }
+    }
+    
+}
