7600723ac203140ac676974c723910d0ac6f963a
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-05-21 15:39:12 +0200

    Added menu 2

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -11,6 +11,7 @@
                 // MENY
                 Console.WriteLine("--- Huvudmeny ---");
                 Console.WriteLine("1. Ålderskategori");
+                Console.WriteLine("2. Totalpris för sällskapet");
                 Console.WriteLine("0. Avsluta");
                 Console.Write("Välj: ");
 
@@ -21,8 +22,9 @@
                     case "1":
                         CheckAge();
                         break;
-
-
+                    case "2":
+                        groupOfPeople();
+                        break;
                     case "0":
                         meny = false;
                         break;
@@ -34,6 +36,32 @@
 
             }
         }
+
+        private static void groupOfPeople()
+        {
+            int totalPris = 0; //startvärde på totalpriset
+            Console.Write("Hur många personer?");
+            int numberOfGuests = int.Parse(Console.ReadLine());
+            Console.WriteLine("För skriv in ålder för varje person:");
+            for (int i = 0; i < numberOfGuests - 1; i++)
+            {
+                Console.Write($"Ålder på person {i+1} :");
+                int guestAge = int.Parse(Console.ReadLine());
+                if (guestAge < 20) 
+                {
+                    totalPris += 80;
+                }
+                else if (guestAge >64)
+                {
+                    totalPris += 90;
+                }
+                else
+                {
+                    totalPris += 120;
+                }
+            }
+            Console.WriteLine($"Antal personer: {numberOfGuests} totalpris : {totalPris} ");
+         }
 
         private static void CheckAge()
         {
