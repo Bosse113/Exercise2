9052a469de4f72a9167b5dacafe9fe9e8462f171
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-05-22 11:24:13 +0200

    Added ToDo for error handling

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //ToDo:bättre och mer noggrann felkontroll på input
             bool meny = true;
 
             while (meny == true)
